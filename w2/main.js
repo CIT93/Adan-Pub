401d3a7ffbd28434340b9b7e60a9e57c713ee8a4
Adan Tolentino Gatica <tolentinoadan030@gmail.com>
2025-01-23 21:52:15 -0800

    coding is done

--- w2/main.js
+++ w2/main.js
@@ -2,22 +2,22 @@
 // Method 1
 
 // 1. Count the members of your household.
-
+const myHousemembers = 6;
 // 2. Consider the size of your home.
-
+const myHousesize = 7;
 // 3. Evaluate your food choices.
-
+const myHousefood = 8;
 // 4. Examine your water consumption.
-
+const myHousewater = 1;
 // 5. Determine how many household purchases you make each year.
-
+const myHousepurchases = 4;
 // 6. Consider how much waste you produce.
-
+const myHousewaste = 30;
 // 7. Identify the amount of waste that you recycle.
-
+const myHouserecycle = 4;
 // 8. Tally up your annual transportation scores.
-
+const myHousetransport = 10;
 // 9. Add up your points.
-
+const cfpTotal = myHousemembers + myHousesize + myHousefood + myHousewater + myHousepurchases + myHousewaste + myHouserecycle + myHousetransport
 // 10. Write 35 to update the rendered html (index.html) with total footprint 
 
