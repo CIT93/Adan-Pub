1c2c4b87f1454ff54cd6d9b30929ef68376771ea
Adan Tolentino Gatica <tolentinoadan030@gmail.com>
2025-01-23 21:04:30 -0800

    setup done

--- w2/main.js
+++ w2/main.js
@@ -1,1 +1,23 @@
-console.log("Week 1 - Hello World");
+// How to Calucate Your Carbon Footprint - https://www.wikihow.com/Calculate-Your-Carbon-Footprint
+// Method 1
+
+// 1. Count the members of your household.
+
+// 2. Consider the size of your home.
+
+// 3. Evaluate your food choices.
+
+// 4. Examine your water consumption.
+
+// 5. Determine how many household purchases you make each year.
+
+// 6. Consider how much waste you produce.
+
+// 7. Identify the amount of waste that you recycle.
+
+// 8. Tally up your annual transportation scores.
+
+// 9. Add up your points.
+
+// 10. Write 35 to update the rendered html (index.html) with total footprint 
+
