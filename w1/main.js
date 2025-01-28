9f77109722b139c536dc23f9e97c512a816e5d73
Adan Tolentino Gatica <tolentinoadan030@gmail.com>
2025-01-27 22:44:28 -0800

    week 3 attendeance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,21 @@
-console.log("Week 1 - Hello World");
+console.log("Hello from inside the main .js file");
+
+//et myVar = ;
+
+const myVar = "Adan";
+const myVarType = typeof(myVar)
+console.log ("myVarType" + myVarType);
+console.log ('myVarType ${myVarType}');
+
+
+
+function runNow () {
+    if (myVarType === "number") {
+        console.log('will 22 this one run?')
+    }else{
+        console.log('will 33 this one run?')
+    }
+}
+
+runNow();
+runNow();
